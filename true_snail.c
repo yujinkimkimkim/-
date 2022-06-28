d2f6db5fb615b82916ae96f454095fac694fe634
yujinkimkimkim <kimyj4852@naver.com>
2022-06-28 16:28:18 +0900

    이게 진짜에요

--- true_snail.c
+++ true_snail.c
@@ -3,7 +3,7 @@
 
 int main() 
 {
-	int input, i, j, m ,cheak=0, l = 0, k = 1, num=1;
+	int input, i, j, m , l = 0, k = 1, num=1;
 	int arr[10][10];
 	int sum = 0, arr1 = 0;
 	scanf("%d", &input);
@@ -17,7 +17,7 @@ int main()
 
 			if (num > input * input)
 			{
-				goto re;
+				continue;
 			}
 
 		}
@@ -28,7 +28,7 @@ int main()
 			
 			if (num > input * input)
 			{
-				goto re;
+				continue;
 			}
 		}
 		for (j = input - k-1; j >= i; j--)
@@ -38,7 +38,7 @@ int main()
 			
 			if (num > input * input)
 			{
-				goto re;
+				continue;
 			}
 		}
 		for (j = input -k-1; j >= k; j--) 
@@ -48,14 +48,14 @@ int main()
 			
 			if (num > input * input)
 			{
-				goto re;
+				continue;
 			}
 		}	
 		k++;
 	}
 
 
-re:
+
 
 	for (i = 0; i < input; i++)
 	{
@@ -69,26 +69,32 @@ re:
 			}
 		}
 	}
-	int arr2, o;
+	
 	
 	for (i = 0; i < input; i++)
 	{
 		l = input - i -1; 
-		o = input - 1;
 		arr1 = arr[l][0]; 
-		arr2 = arr[o][i+1];
+		
 			for (j = 1; j <= input - i - 1; j++)
 			{
 				arr1 *= arr[--l][j];
 			}
-			for (j = i +1; j <= input - 1; j++)
-			{
-				arr2 *= arr[--o][j];
-			}
-			sum = arr2 + arr1;
+			sum += arr1;				
+	}
+	int arr2, o;
+	for (i = 0; i < input - 1; i++)
+	{
+		o = input - 1;
+		arr2 = arr[o][i + 1];
+		for (j = i + 1; j < input - 1; j++)
+		{
+			arr2 *= arr[--o][j+1];
+		}
+		sum += arr2;
 	}
 	 
-	printf("대각선 곱 합(n-1케이스까지): %d", sum);
+	printf("대각선 곱 합: %d", sum);
 
 	return 0;
 }
